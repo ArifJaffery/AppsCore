08a5ff43ce4f722091257255ce4183b3c436931d
ArifJffery <jaff_arif@yahoo.com>
2017-12-29 20:40:15 +1100

    Create API's

---
+++ src/api.ts
@@ -0,0 +1,14 @@
+export interface iplace {
+    id:number;
+    name:number;
+}
+
+export interface ipeople{
+    id:number;
+    name:number;
+    gender:string;
+    father_id:number;
+    mother_id:number;
+    place_id:number;
+    level:number;
+}
